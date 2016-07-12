c077bc2454cffc1f42c14f6cd47f45b7e9714e9c
nimojs <nimo.jser@gmail.com>
2016-07-12 14:53:36 +0800

    markrun title

--- fis-conf.js
+++ fis-conf.js
@@ -15,7 +15,7 @@ fis.match('README.md', {
                 '<meta charset="UTF-8">',
                 '<meta name="viewport" content="width=device-width, initial-scale=1.0">',
                 '<meta http-equiv="X-UA-Compatible" content="ie=edge">',
-                '<title>Document</title>',
+                '<title><%- title %></title>',
             '</head>',
             '<body>',
             '<%- content %>',
